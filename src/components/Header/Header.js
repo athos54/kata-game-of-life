7282e1b910d4cef991e24bf660020b5b960657a8
agenteMike <ericsantoangel@gmail.com>
2022-01-11 19:30:35 +0100

    Create input and button test

---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+export function Header () {
+    return (
+        <input type='number' name='size' placeholder='Introduce the size of table' />
+    )
+}
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { Header } from "./Header.js";
 
 describe("header component", () => {
   it("input should contain placeholder eq to Introduce the size of table", () => {
@@ -8,4 +10,11 @@ describe("header component", () => {
       screen.getByPlaceholderText("Introduce the size of table")
     ).toBeInTheDocument();
   });
+
+  it("when click on start element header should disappear", () => {
+    render(<Header />);
+
+    userEvent.click(screen.getByText('Start!'))
+    expect(screen.getByText('Start!')).not.toBeInTheDocument()
+  })
 });
